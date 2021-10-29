2f5d53e0fbd24fe4c5e83b1d274947bb76c30db9
M1TE5H <mp3722@columbia.edu>
2021-10-29 03:31:56 +0100

    Update zairyo.rb

--- lib/zairyo.rb
+++ lib/zairyo.rb
@@ -1,8 +1,13 @@
 module Zairyo
 	class << self
 		def parseMaterial (m_id, m_name)
-			@material_id = m_id;
-			@material_name = m_name;
+			@material_id = m_id
+			@material_name = m_name
+		end
+
+		def printMaterial ()
+			puts "\nMaterial Identification:\t#{m_id}"
+			puts "\nMaterial Name:\t#{m_name}"
 		end
 	end
 end
